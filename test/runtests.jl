19e36e8d7aa922f41276033d6029d2f3e7fd8460
Carleton <cjc@lanl.gov>
2016-11-14 17:51:41 -0700

    removing comments from ml tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,13 +26,8 @@ end
 ipopt_solver = IpoptSolver(tol=1e-6, print_level=0)
 pajarito_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=ipopt_solver, log_level=0)
 
+scs_solver = SCSSolver(max_iters=1000000, verbose=0)
+pajarito_sdp_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=scs_solver, log_level=0)
 
-result = run_ml("../test/data/case3_ml.m", ACPPowerModel, ipopt_solver)
-display(result)
-display(result["solution"]["bus"])
-
-#scs_solver = SCSSolver(max_iters=1000000, verbose=0)
-#pajarito_sdp_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=scs_solver, log_level=0)
-
-#include("ml.jl")
-#include("mluc.jl")
+include("ml.jl")
+include("mluc.jl")
