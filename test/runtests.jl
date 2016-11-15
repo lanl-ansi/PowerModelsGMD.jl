119e221c098b8e521aa6eddc9e814b5eae024e15
Carleton <cjc@lanl.gov>
2016-11-14 17:50:48 -0700

    adding ipopt dependencies to travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,8 +26,13 @@ end
 ipopt_solver = IpoptSolver(tol=1e-6, print_level=0)
 pajarito_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=ipopt_solver, log_level=0)
 
-scs_solver = SCSSolver(max_iters=1000000, verbose=0)
-pajarito_sdp_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=scs_solver, log_level=0)
 
-include("ml.jl")
-include("mluc.jl")
+result = run_ml("../test/data/case3_ml.m", ACPPowerModel, ipopt_solver)
+display(result)
+display(result["solution"]["bus"])
+
+#scs_solver = SCSSolver(max_iters=1000000, verbose=0)
+#pajarito_sdp_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=scs_solver, log_level=0)
+
+#include("ml.jl")
+#include("mluc.jl")
