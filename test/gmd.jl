008d21633d2bf7e37b6275f6a690da7a6ef621c2
Arthur Barnes <abarnes@lanl.gov>
2017-02-24 13:50:15 -0800

    fixed test for 4-bus system

--- test/gmd.jl
+++ test/gmd.jl
@@ -2,10 +2,23 @@
 
 @testset "test ac gmd" begin
     @testset "4-bus case" begin
-        result = run_ac_opf("../test/data/b4gic.json", ipopt_solver)
+        data = PowerModels.parse_file("/home/abarnes/.julia/v0.5/PowerModelsGMD/test/data/b4gic.json")
+        data["do_gmd"] = true
+        data = PowerModelsGMD.setup_gmd(data)
+        # data = PowerModels.parse_file("../test/data/b4gic.json")
+        pm = PowerModels.ACPPowerModel(data,solver=IpoptSolver())
+        pm.setting["output"] = Dict("line_flows" => true)
+        PowerModelsGMD.post_gmd(pm)
+        status, solve_time = solve(pm)
+        result = PowerModels.build_solution(pm, status, solve_time; solution_builder = PowerModelsGMD.get_gmd_solution)
+
+        if !(result["status"] === :LocalInfeasible)
+            data = PowerModelsGMD.merge_result(data,result)
+        end
+        # result = run_ml("../test/data/case3_ml.m", ACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
-        @test isapprox(result["objective"], 139800.45613240905; atol = 1e3)
+        @test isapprox(result["objective"], 1.398e5; atol = 1e2)
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
+using Ipopt
 using PowerModels
+using PowerModelsGMD
 using Logging
 # suppress warnings during testing
 Logging.configure(level=ERROR)
 
-using Ipopt
 
 if VERSION >= v"0.5.0-dev+7720"
     using Base.Test
@@ -15,11 +16,5 @@ end
 # default setup for solvers
 ipopt_solver = IpoptSolver(tol=1e-6, print_level=0)
 
-# used by OTS and Loadshed TS models
-function check_br_status(sol)
-    for (idx,val) in sol["branch"]
-        @test val["br_status"] == 0.0 || val["br_status"] == 1.0
-    end
-end
 
 include("gmd.jl")
