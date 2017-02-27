7279a1f0ad52e25748d58db4116084f5ebe33f3f
ccoffrin <carleton@coffrin.com>
2017-02-27 11:39:15 -0700

    removing make_per_unit functions

--- src/core/base.jl
+++
@@ -1,8 +0,0 @@
-
-function PMs.make_per_unit(mva_base::Number, data::Void)
-    #nothing to do
-end
-
-function PMs.make_per_unit(mva_base::Number, data::Tuple{Int64,Int64,Int64})
-    #nothing to do
-end
