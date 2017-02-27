9986c49a89d49a89b4786a824400118504ddd3b4
ccoffrin <carleton@coffrin.com>
2017-02-27 10:59:05 -0700

    adding PowerModels version upper bound, adding missing make_per_unit functions

---
+++ src/core/base.jl
@@ -0,0 +1,8 @@
+
+function PMs.make_per_unit(mva_base::Number, data::Void)
+    #nothing to do
+end
+
+function PMs.make_per_unit(mva_base::Number, data::Tuple{Int64,Int64,Int64})
+    #nothing to do
+end
