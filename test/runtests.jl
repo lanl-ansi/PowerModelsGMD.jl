0b95254968563dabdb0e8e763522f08a6eb19af9
Russell Bent <rbent@lanl.gov>
2018-01-23 10:16:36 -0700

    Turned on all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,6 +26,6 @@ pajarito_solver = PajaritoSolver(mip_solver=cbc_solver, cont_solver=ipopt_solver
 
 setting = Dict{AbstractString,Any}("output" => Dict{AbstractString,Any}("branch_flows" => true))
 
-#include("gmd.jl")
-#include("gmd_ls.jl")
+include("gmd.jl")
+include("gmd_ls.jl")
 include("gmd_ots.jl")
