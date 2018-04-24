ec0324427c12b6f339a3c2cf1b9a5458d27ab1aa
carleton <cjc@lanl.gov>
2018-04-24 17:33:20 -0600

    update function type constraint syntax

--- src/form/acp.jl
+++ src/form/acp.jl
@@ -1,29 +1,26 @@
 
 
-""
-const ACPPowerModel = GenericPowerModel{PowerModels.StandardACPForm}
-
 "default AC constructor for GMD type problems"
 ACPPowerModel(data::Dict{String,Any}; kwargs...) =
     GenericGMDPowerModel(data, PowerModels.StandardACPForm; kwargs...)
 
 ""
-function variable_ac_current{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T},n::Int=pm.cnw; bounded = true)
+function variable_ac_current(pm::GenericPowerModel{T},n::Int=pm.cnw; bounded = true) where T <: PowerModels.AbstractACPForm
    variable_ac_current_mag(pm,n;bounded=bounded)
 end
 
 ""
-function variable_ac_current_on_off{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T},n::Int=pm.cnw)
+function variable_ac_current_on_off(pm::GenericPowerModel{T},n::Int=pm.cnw) where T <: PowerModels.AbstractACPForm
    variable_ac_current_mag(pm,n;bounded=false) # needs to be false because this is an on/off variable
 end
 
 ""
-function variable_dc_current{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T},n::Int=pm.cnw; bounded = true)
+function variable_dc_current(pm::GenericPowerModel{T},n::Int=pm.cnw; bounded = true) where T <: PowerModels.AbstractACPForm
    variable_dc_current_mag(pm,n;bounded=bounded)
 end
 
 ""
-function variable_reactive_loss{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T},n::Int=pm.cnw; bounded = true)
+function variable_reactive_loss(pm::GenericPowerModel{T},n::Int=pm.cnw; bounded = true) where T <: PowerModels.AbstractACPForm
    variable_qloss(pm,n;bounded=bounded)
 end    
      
@@ -33,7 +30,7 @@ sum(p[a] for a in bus_arcs)  == sum(pg[g] for g in bus_gens) - pd - gs*v^2 + pd_
 sum(q[a] for a in bus_arcs)  == sum(qg[g] for g in bus_gens) - qd + bs*v^2 + qd_ls - qloss
 ```
 """
-function constraint_kcl_shunt_gmd_ls{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T}, n::Int, i::Int, bus_arcs, bus_arcs_dc, bus_gens, bus_loads, bus_shunts, pd, qd, gs, bs)
+function constraint_kcl_shunt_gmd_ls(pm::GenericPowerModel{T}, n::Int, i::Int, bus_arcs, bus_arcs_dc, bus_gens, bus_loads, bus_shunts, pd, qd, gs, bs) where T <: PowerModels.AbstractACPForm
     vm = pm.var[:nw][n][:vm][i]
     p = pm.var[:nw][n][:p]
     q = pm.var[:nw][n][:q]
@@ -48,7 +45,7 @@ function constraint_kcl_shunt_gmd_ls{T <: PowerModels.AbstractACPForm}(pm::Gener
 end
 
 "Constraint for relating current to power flow"
-function constraint_current{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T}, n::Int, i, f_idx, f_bus, t_bus, tm)
+function constraint_current(pm::GenericPowerModel{T}, n::Int, i, f_idx, f_bus, t_bus, tm) where T <: PowerModels.AbstractACPForm
     i_ac_mag = pm.var[:nw][n][:i_ac_mag][i] 
     p_fr     = pm.var[:nw][n][:p][f_idx]
     q_fr     = pm.var[:nw][n][:q][f_idx]
@@ -58,7 +55,7 @@ function constraint_current{T <: PowerModels.AbstractACPForm}(pm::GenericPowerMo
 end
 
 "Constraint for relating current to power flow on/off"
-function constraint_current_on_off{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T}, n::Int, i, ac_max)
+function constraint_current_on_off(pm::GenericPowerModel{T}, n::Int, i, ac_max) where T <: PowerModels.AbstractACPForm
     z  = pm.var[:nw][n][:branch_z][i]
     i_ac = pm.var[:nw][n][:i_ac_mag][i]        
     @constraint(pm.model, i_ac <= z * ac_max)
@@ -66,7 +63,7 @@ function constraint_current_on_off{T <: PowerModels.AbstractACPForm}(pm::Generic
 end
 
 "Constraint for computing thermal protection of transformers"
-function constraint_thermal_protection{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T}, n::Int, i, coeff, ibase)
+function constraint_thermal_protection(pm::GenericPowerModel{T}, n::Int, i, coeff, ibase) where T <: PowerModels.AbstractACPForm
     i_ac_mag = pm.var[:nw][n][:i_ac_mag][i] 
     ieff = pm.var[:nw][n][:i_dc_mag][i] 
 
@@ -74,7 +71,7 @@ function constraint_thermal_protection{T <: PowerModels.AbstractACPForm}(pm::Gen
 end
 
 "Constraint for computing qloss"
-function constraint_qloss{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T}, n::Int, k, i, j, K, branchMVA)
+function constraint_qloss(pm::GenericPowerModel{T}, n::Int, k, i, j, K, branchMVA) where T <: PowerModels.AbstractACPForm
     qloss = pm.var[:nw][n][:qloss]
     i_dc_mag = pm.var[:nw][n][:i_dc_mag][k]
     vm = pm.var[:nw][n][:vm][i]
@@ -89,7 +86,7 @@ function constraint_qloss{T <: PowerModels.AbstractACPForm}(pm::GenericPowerMode
 end
 
 "Constraint for computing qloss"
-function constraint_qloss{T <: PowerModels.AbstractACPForm}(pm::GenericPowerModel{T}, n::Int, k, i, j)
+function constraint_qloss(pm::GenericPowerModel{T}, n::Int, k, i, j) where T <: PowerModels.AbstractACPForm
     qloss = pm.var[:nw][n][:qloss]    
     @constraint(pm.model, qloss[(k,i,j)] == 0.0)
     @constraint(pm.model, qloss[(k,j,i)] == 0.0)
