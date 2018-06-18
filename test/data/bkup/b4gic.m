64692bf86b8879f566cc2734c27dc9be5df080bd
Russell Bent <rbent@lanl.gov>
2018-06-18 13:32:41 -0600

    cleaned up the matpower format according to cjc suggestions
    Matpower format cleanup

--- test/data/bkup/b4gic.m
+++ test/data/bkup/b4gic.m
@@ -23,9 +23,9 @@ mpc.gen = [
 %% branch data
 %    fbus    tbus    r    x    b    rateA    rateB    rateC    ratio    angle    status    angmin    angmax
 mpc.branch = [
-	1	3	0.0001	0.004	0	9000.0	0.0	0.0	1	0.0	1	-30.0	30.0
-	1	2	0.000513	0.01	0	9000.0	0.0	0.0	1	0.0	1	-30.0	30.0
-	2	4	0.0001	0.004	0	9000.0	0.0	0.0	1	0.0	1	-30.0	30.0
+	1	3	0.0001	0.004	0	9000.0	0.0	0.0	1	0.0	1	-29.999999999999996	29.999999999999996
+	1	2	0.000513	0.01	0	9000.0	0.0	0.0	1	0.0	1	-29.999999999999996	29.999999999999996
+	2	4	0.0001	0.004	0	9000.0	0.0	0.0	1	0.0	1	-29.999999999999996	29.999999999999996
 ];
 
 %%-----  OPF Data  -----%%
@@ -36,35 +36,51 @@ mpc.gencost = [
 	2	0	0	3	0.11	5.0	0
 ];
 
-%column_names% parent_index g_gnd name
-mpc.gmd_bus = {
-	1	5	'dc_sub1'	
-	2	5	'dc_sub2'	
-	1	0	'dc_bus1'	
-	2	0	'dc_bus2'	
-	3	0	'dc_bus3'	
-	4	0	'dc_bus4'	
+%column_names% parent_index g_gnd
+mpc.gmd_bus = [
+	1	5	
+	2	5	
+	1	0	
+	2	0	
+	3	0	
+	4	0	
+];
+
+%column_names%  name
+mpc.gmd_bus_strings = {
+	'dc_sub1'	
+	'dc_sub2'	
+	'dc_bus1'	
+	'dc_bus2'	
+	'dc_bus3'	
+	'dc_bus4'	
 };
 
-%column_names%  f_bus t_bus parent_index br_status br_r br_v len_km name
-mpc.gmd_branch = {
-	3	1	1	1	0.1	0	0	'dc_xf1_hi'	
-	3	4	2	1	1.00073475	170.78806587354	170.78806587354	'dc_br1'	
-	4	2	3	1	0.1	0	0	'dc_xf2_hi'	
+%column_names%  f_bus t_bus parent_index br_status br_r br_v len_km
+mpc.gmd_branch = [
+	3	1	1	1	0.1	0	0	
+	3	4	2	1	1.00073475	170.78806587354	170.78806587354	
+	4	2	3	1	0.1	0	0	
+];
+
+%column_names%  name
+mpc.gmd_branch_strings = {
+	'dc_xf1_hi'	
+	'dc_br1'	
+	'dc_xf2_hi'	
 };
 
-%column_names%  hi_bus lo_bus gmd_br_hi gmd_br_lo gmd_k gmd_br_series gmd_br_common baseMVA type config
-mpc.branch_gmd = {
-	1	3	1	-1	1.793	-1	-1	100	'xf'	'gwye-delta'	
-	1	2	-1	-1	0	-1	-1	100	'line'	'none'	
-	2	4	3	-1	1.793	-1	-1	100	'xf'	'gwye-delta'	
-};
+%column_names%  hi_bus lo_bus gmd_br_hi gmd_br_lo gmd_k gmd_br_series gmd_br_common baseMVA
+mpc.branch_gmd = [
+	1	3	1	-1	1.793	-1	-1	100	
+	1	2	-1	-1	0	-1	-1	100	
+	2	4	3	-1	1.793	-1	-1	100	
+];
 
-%column_names%  lat lon
-mpc.bus_gmd = {
-	40	-89	
-	40	-87	
-	40	-89	
-	40	-87	
+%column_names%  type config
+mpc.branch_gmd_strings = {
+	'xf'	'gwye-delta'	
+	'line'	'none'	
+	'xf'	'gwye-delta'	
 };
 
