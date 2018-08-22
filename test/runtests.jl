2a675d5f50eaf006fb3596f1dd1a461459ac28b8
carleton <cjc@lanl.gov>
2018-08-22 00:10:30 -0600

    update to pavito solver

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Ipopt
 using PowerModels
 #using CPLEX
 using PowerModelsGMD
-using Pajarito
+using Pavito
 using Cbc
 using Juniper
 using Memento
@@ -25,7 +25,7 @@ juniper_solver = JuniperSolver(ipopt_solver, mip_solver=cbc_solver, log_levels=[
 
 #gurobi_solver = GurobiSolver(OutputFlag=0) # change to Pajarito
 #cplex_solver = CplexSolver()
-pajarito_solver = PajaritoSolver(mip_solver=cbc_solver, cont_solver=ipopt_solver, log_level=1)
+pavito_solver = PavitoSolver(mip_solver=cbc_solver, cont_solver=ipopt_solver, mip_solver_drives=false, log_level=1)
 
 
 setting = Dict{AbstractString,Any}("output" => Dict{AbstractString,Any}("branch_flows" => true))
