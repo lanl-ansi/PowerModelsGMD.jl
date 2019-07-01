ea1ef565fcd354c7468906883a4290f9a594698d
Arthur Barnes <artbarnes@ieee.org>
2019-07-01 09:05:39 -0700

    got gmd_opf_ts_decoupled running

--- src/prob/gmd_opf_ts_decoupled.jl
+++ src/prob/gmd_opf_ts_decoupled.jl
@@ -1,72 +1,52 @@
 # Formulations of GMD Problems
-export run_ac_gic_opf_ts_decoupled
+export run_ac_gmd_opf_ts_decoupled
 
 
-function modify_gic_case(gic_case, mods, time_index) 
-    gic_branches = Dict()
-
-    for gb in values(gic_case["gic_branch"])
-        k = gb["parent_index"]
-        gic_branches[k] gb
+function modify_gmd_case!(net, mods, time_index) 
+    for (k,wf) in mods["waveforms"]
+        otype = wf["parent_type"]
+        field  = wf["parent_field"]
+        net[otype][k][field] = wf["values"][time_index]
     end
 
-    for omod in values(mods["object_modifiers"])
-        k = omod["parent_index"]
-        gic_branches[k]["Vdc"] = omod["values"][time_index]
-    end
 
-    return gic_case
+    return net
 end
         
 
-function run_ac_gic_opf_ts_decoupled(dc_case, solver, mods, settings; kwargs...)
-    ac_case = deepcopy(dc_case)
-
+function run_ac_gmd_opf_ts_decoupled(net, solver, mods, settings; kwargs...)
     # get the number of time steps
-    t = mods["times"]
+    t = mods["time"]
     N = length(t)
-    ts_data = []
-    Delta_t = t[2] - t[1]
+    results = []
 
     for n in 1:N
-        modify_gic_case(dc_case, mods, n)
-        dc_result = PowerModelsGMD.run_gic(dc_case, solver; setting=settings)
-        dc_solution = dc_result["solution"]
-        make_gmd_mixed_units(dc_solution, 100.0)
+        modify_gmd_case!(net, mods, n)
+        data = PowerModelsGMD.run_ac_gmd_opf_decoupled(net, solver; setting=settings)
+        data["time_index"] = n
+        data["time"] = t[n]
+        push!(results, data)
 
-        for (k,br) in ac_case["branch"]
-            dc_current_mag(br, ac_case, dc_solution)
-        end
+        Delta_t = t[2] - t[1]
 
-        ac_result = run_ac_opf_qloss(ac_case, solver, setting=settings)
-        adjust_gmd_phasing(dc_result)
-
-        # the data requirements here could be prohibitive
-        data = Dict()
-        data["time"] = mods["times"][n]
-        data["ac"] = Dict("case"=>ac_case, "result"=>ac_result)
-        data["dc"] = Dict("case"=>dc_case, "result"=>dc_result)
-
-
-        if n == 1
-            Delta_t = t[2] - t[1]
-        else
+        if n > 1
             Delta_t = t[n] - t[n-1]
         end
 
-        if "output" in settings && "transformer_temperatures" in settings["output"] && settings["output"] == true
-            top_oil_rise(br, result; Delta_t = Delta_t, base_mva = net["baseMVA"]))
-            update_top_oil_rise(br, net)
+        for (k,br) in data["ac"]["case"]["branch"]
+            if !(br["transformer"] == true)
+                continue
+            end
 
+            result = data["ac"]["result"]
+            top_oil_rise(br, result; base_mva=net["baseMVA"], Delta_t = Delta_t)
+            update_top_oil_rise(br, net)
             ss_hotspot_rise(br, result)
             update_hotspot_rise(br, net)
         end
-
-
-        push!(ts_data, data) 
     end
 
-    return ts_data
+    return results
 end
 
 
