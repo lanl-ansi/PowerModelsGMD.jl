204ddf8564a6e93fbb077b217e46c361623c120d
ccoffrin <carleton@coffrin.com>
2019-07-24 14:58:39 -0600

    fixes to docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,13 @@
 using Documenter, PowerModelsGMD
 
-
-makedocs()
+makedocs(
+    modules = [PowerModelsGMD],
+    sitename = "PowerModelsGMD",
+    authors = "Arthur Barnes, and contributors.",
+    pages = [
+        "Home" => "index.md",
+    ]
+)
 
 deploydocs(
     repo = "github.com/lanl-ansi/PowerModelsGMD.jl.git",
