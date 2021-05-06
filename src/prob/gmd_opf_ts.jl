447550d1a37525843f267c0d24d2bf5b5563d7e4
Adam Mate <amate@lanl.gov>
2021-05-06 14:34:48 -0600

    fixing gmd_opf_ts

--- src/prob/gmd_opf_ts.jl
+++ src/prob/gmd_opf_ts.jl
@@ -1,102 +1,118 @@
-export run_gmd_opf_ts, run_ac_gmd_opf_ts
+export run_ac_gmd_opf_ts
+export run_gmd_opf_ts
 
 
-"FUNCTION: convinience function"
-function run_gmd_opf_ts(data, model_type::Type, optimizer; kwargs...)
-    return PMs.run_model(data, model_type, optimizer, post_gmd_opf_ts; ref_extensions=[ref_add_core!], solution_builder = solution_gmd_ts!, multinetwork=true, kwargs...)
+"FUNCTION: run multi-time-series GMD model with nonlinear ac equations"
+function run_ac_gmd_opf_ts(file, optimizer; kwargs...)
+    return run_gmd_opf_ts(
+        file,
+        _PM.ACPPowerModel,
+        optimizer;
+        kwargs...,
+    )
+end
+
+function run_gmd_opf_ts(file, model_type::Type, optimizer; kwargs...)
+    return _PM.run_model(
+        file,
+        model_type,
+        optimizer,
+        build_gmd_opf_ts;
+        multinetwork = true,
+        ref_extensions = [
+            ref_add_gmd!
+        ],
+        solution_processors = [
+            solution_gmd!,
+            solution_PM!,
+            solution_gmd_qloss!,
+            solution_gmd_mls!,
+            solution_gmd_xfmr_temp!
+        ],
+        kwargs...,
+    )
 end
 
 
-"FUNCTION: GMD OPF TS problem specification"
-function post_gmd_opf_ts(pm::PMs.AbstractPowerModel; kwargs...)
+"FUNCTION: build the multi-time-series ac optimal power flow coupled with quasi-dc power flow problem
+as a transformer heating minimization problem"
+function build_gmd_opf_ts(pm::_PM.AbstractPowerModel; kwargs...)
 
-    for (n, network) in PMs.nws(pm)
+    for (n, network) in _PM.nws(pm)
 
-        # -- Variables -- #
-
-        PMs.variable_bus_voltage(pm, nw=n)
-        PMs.variable_gen_power(pm, nw=n)
-        PMs.variable_branch_power(pm, nw=n)
-        PMs.variable_dcline_power(pm, nw=n)
+        _PM.variable_bus_voltage(pm, nw=n)
+        _PM.variable_gen_power(pm, nw=n)
+        _PM.variable_branch_power(pm, nw=n)
+        _PM.variable_dcline_power(pm, nw=n)
 
         variable_dc_voltage(pm, nw=n)
         variable_dc_current_mag(pm, nw=n)
-        variable_qloss(pm, nw=n)
         variable_dc_line_flow(pm, nw=n)
+        variable_qloss(pm, nw=n)
 
-        b = true
-        variable_delta_oil_ss(pm, nw=n, bounded=b)
-        variable_delta_oil(pm, nw=n, bounded=b)
-        variable_delta_hotspot_ss(pm, nw=n, bounded=b)
-        variable_delta_hotspot(pm, nw=n, bounded=b)
-        variable_hotspot(pm, nw=n, bounded=b)
+        variable_delta_oil_ss(pm, nw=n, bounded=true)
+        variable_delta_oil(pm, nw=n, bounded=true)
+        variable_delta_hotspot_ss(pm, nw=n, bounded=true)
+        variable_delta_hotspot(pm, nw=n, bounded=true)
+        variable_hotspot(pm, nw=n, bounded=true)
 
-        # -- Constraints -- #
+        _PM.constraint_model_voltage(pm, nw=n)
 
-        # - General - #
-
-        PMs.constraint_model_voltage(pm, nw=n)
-
-        for i in PMs.ids(pm, :ref_buses, nw=n)
-            PMs.constraint_theta_ref(pm, i, nw=n)
+        for i in _PM.ids(pm, :ref_buses, nw=n)
+            _PM.constraint_theta_ref(pm, i, nw=n)
         end
 
-        for i in PMs.ids(pm, :bus, nw=n)
+        for i in _PM.ids(pm, :bus, nw=n)
             constraint_power_balance_gmd(pm, i, nw=n)
         end
 
-        for i in PMs.ids(pm, :branch, nw=n)
-            constraint_dc_current_mag(pm, i, nw=n)
+        for i in _PM.ids(pm, :branch, nw=n)
+
+            _PM.constraint_ohms_yt_from(pm, i, nw=n)
+            _PM.constraint_ohms_yt_to(pm, i, nw=n)
+
+            _PM.constraint_voltage_angle_difference(pm, i, nw=n)
+
+            _PM.constraint_thermal_limit_from(pm, i, nw=n)
+            _PM.constraint_thermal_limit_to(pm, i, nw=n)
+
             constraint_qloss_vnom(pm, i, nw=n)
+            constraint_dc_current_mag(pm, i, nw=n)
 
-            PMs.constraint_ohms_yt_from(pm, i, nw=n)
-            PMs.constraint_ohms_yt_to(pm, i, nw=n)
+            constraint_temperature_state_ss(pm, i, nw=n)
+            constraint_hotspot_temperature_state_ss(pm, i, nw=n)
+            constraint_hotspot_temperature_state(pm, i, nw=n)
+            constraint_absolute_hotspot_temperature_state(pm, i, nw=n)
 
-            PMs.constraint_voltage_angle_difference(pm, i, nw=n)
-
-            PMs.constraint_thermal_limit_from(pm, i, nw=n)
-            PMs.constraint_thermal_limit_to(pm, i, nw=n)
-
-            constraint_temperature_state_ss(pm, i, nw=n) 
-            constraint_hotspot_temperature_state_ss(pm, i, nw=n)             
-            constraint_hotspot_temperature_state(pm, i, nw=n)                         
-            constraint_absolute_hotspot_temperature_state(pm, i, nw=n)            
         end
 
-        # - DC network - #
-
-        for i in PMs.ids(pm, :gmd_bus)
+        for i in _PM.ids(pm, :gmd_bus, nw=n)
             constraint_dc_power_balance_shunt(pm, i, nw=n)
         end
 
-        for i in PMs.ids(pm, :gmd_branch)
+        for i in _PM.ids(pm, :gmd_branch, nw=n)
             constraint_dc_ohms(pm, i, nw=n)
         end
 
-        for i in PMs.ids(pm, :dcline, nw=n)
-            PMs.constraint_dcline(pm, i, nw=n)
+        for i in _PM.ids(pm, :dcline, nw=n)
+            _PM.constraint_dcline_power_losses(pm, i, nw=n)
         end
 
     end
 
     network_ids = sort(collect(nw_ids(pm)))
-
     n_1 = network_ids[1]
-    for i in ids(pm, :branch, nw=n_1)
+    for i in _PM.ids(pm, :branch, nw=n_1)
         constraint_temperature_state(pm, i, nw=n_1)
     end
-
     for n_2 in network_ids[2:end]
-        for i in ids(pm, :branch, nw=n_2)
+        for i in _PM.ids(pm, :branch, nw=n_2)
             constraint_temperature_state(pm, i, n_1, n_2)
         end
         n_1 = n_2
     end
 
-    # -- Objective -- #
-
     objective_gmd_min_transformer_heating(pm)
 
 end
-
 
--- src/prob/gmd_opf_ts_decoupled.jl
+++ src/prob/gmd_opf_ts_decoupled.jl
@@ -1,98 +1,99 @@
 export run_ac_gmd_opf_ts_decoupled
 
 
-"FUNCTION: update the vs values"
-function modify_gmd_case!(net, mods, time_index)
+"FUNCTION: run the multi-time-series sequential quasi-dc power flow and ac optimal power flow problem"
+function run_ac_gmd_opf_ts_decoupled(case, optimizer, waveform; setting=Dict{String,Any}(), disable_thermal=true, kwargs...)
 
-    if mods !== nothing && mods["waveforms"] !== nothing
+    wf_time = waveform["time"]
+    wf_waveforms = waveform["waveforms"]
 
-        for (k,wf) in mods["waveforms"]
+    if disable_thermal == false
+
+        base_mva = case["baseMVA"]
+        delta_t = wf_time[2]-wf_time[1]
+    
+        Ie_prev = Dict()
+        for (i, br) in case["branch"]
+            Ie_prev[i] = nothing
+        end
+
+    end
+
+
+    # Run each timestep:
+
+    solution = []
+    for i in 1:length(wf_time)
+
+        update_gmd_case!(case, waveform, i)
+
+        result = run_ac_gmd_opf_decoupled(case, optimizer; setting=setting)
+        result["time_index"] = i
+        result["time"] = wf_time[i]
+
+        if disable_thermal == false
+
+            xfmr_temp = Dict("Ieff" => 0.0, "delta_topoilrise_ss" => 0.0, "delta_hotspotrise_ss" => 0.0, "actual_hotspot" => 0.0)
+
+            if i > 1
+                delta_t = wf_time[i] - wf_time[i-1]
+            end
+
+            for (k, br) in result["ac"]["case"]["branch"]
+
+                ac_result = result["ac"]["result"]
+
+                if !(br["type"] == "xfmr" || br["type"] == "xf" || br["type"] == "transformer")
+                    continue
+
+                else
+
+                    delta_topoilrise_ss(br, ac_result, base_mva)
+                    delta_topoilrise(br, ac_result, base_mva, delta_t)
+                    update_topoilrise(br, case)
+
+                    delta_hotspotrise_ss(br, ac_result)
+                    delta_hotspotrise(br, ac_result, Ie_prev[k], delta_t)
+                    update_hotspotrise(br, case)
+
+                    xfmr_temp["Ieff"] = br["ieff"]
+                    # xfmr_temp["delta_topoilrise"] = br["delta_topoilrise"]
+                    xfmr_temp["delta_topoilrise_ss"] = br["delta_topoilrise_ss"]
+                    # xfmr_temp["delta_hotspotrise"] =  br["delta_hotspotrise"]
+                    xfmr_temp["delta_hotspotrise_ss"] = br["delta_hotspotrise_ss"]
+                    xfmr_temp["actual_hotspot"] = (br["temperature_ambient"] + br["delta_topoilrise_ss"] + br["delta_hotspotrise_ss"])
+
+                end
+
+                merge!(ac_result["solution"]["branch"]["$k"], xfmr_temp)
+
+            end
+
+        end
+
+        push!(solution, result)
+
+    end
+
+    return solution
+
+end
+
+
+"FUNCTION: update values in the case"
+function update_gmd_case!(case, waveform, time_index)
+
+    if (waveform !== nothing && waveform["waveforms"] !== nothing)
+        for (k, wf) in waveform["waveforms"]
+
             otype = wf["parent_type"]
             field  = wf["parent_field"]
-            net[otype][k][field] = wf["values"][time_index]
-        end
+            case[otype][k][field] = wf["values"][time_index]
 
+        end
     end
-    return net
+
+    return case
 
 end
-
-
-"FUNCTION: decoupled time-extended GMD+OPF specification"
-function run_ac_gmd_opf_ts_decoupled(net, optimizer, mods, settings; kwargs...)
-
-    timesteps = mods["time"]
-    n = length(timesteps)
-    t = timesteps
-
-    # Input values for temperature calculations
-    base_mva = net["baseMVA"]
-    delta_t = t[2]-t[1]
-
-    results = []
-    Ie_prev = Dict()
-    
-    for (k,br) in net["branch"]
-        Ie_prev[k] = nothing
-    end
-
-    # println("Start running each time periods\n")
-    # for i in 1:n
-    for i in 21:23
-
-        # println("")
-        # println("########## Time: $(t[i]) ##########")
-
-        modify_gmd_case!(net, mods, i)
-
-        # println("Run AC-OPF using calculated quasi-dc currents")
-        data = run_ac_gmd_opf_decoupled(net, optimizer; setting=settings)
-        
-        data["time_index"] = i
-        data["time"] = t[i]
-        trf_temp = Dict("Ieff"=>0.0, "delta_topoilrise_ss"=>0.0, "delta_hotspotrise_ss"=>0.0, "actual_hotspot"=>0.0)
-
-        if i > 1
-            delta_t = t[i] - t[i-1]
-        end
-
-        for (k,br) in data["ac"]["case"]["branch"]
-
-            if !(br["type"] == "transformer" || br["type"] == "xf")
-                continue
-            end
-
-            result = data["ac"]["result"]
-           
-            if false  
-                delta_topoilrise(br, result, base_mva, delta_t) 
-                # delta_topoilrise_ss(br, result, base_mva) #included in delta_topoilrise
-                update_topoilrise(br, net)
-                
-                # delta_hotspotrise(br, result, Ie_prev[k], delta_t) #decided to only calculate stead-state value
-                delta_hotspotrise_ss(br, result)
-                update_hotspotrise(br, net)
-                
-                # Store calculated transformer temperature related results:
-                trf_temp["Ieff"] = br["ieff"]
-                #trf_temp["delta_topoilrise"] = br["delta_topoilrise"] #decided not to store value
-                trf_temp["delta_topoilrise_ss"] = br["delta_topoilrise_ss"]
-                #trf_temp["delta_hotspotrise"] =  br["delta_hotspotrise"] #decided not to store value
-                trf_temp["delta_hotspotrise_ss"] = br["delta_hotspotrise_ss"]
-                trf_temp["actual_hotspot"] = (br["temperature_ambient"]+br["delta_topoilrise_ss"]+br["delta_hotspotrise_ss"])
-            end
-                
-            merge!(result["solution"]["branch"][k], trf_temp)
-
-
-        end
-        
-        push!(results, data)
-        
-    end
-    # println("Finished running\n")
-
-    return results
-end
-
 
