b3bd68c0d8c25d040d08b1c7322adca6f5b18ef0
Art Barnes <art@pin2.io>
2022-10-15 10:47:30 -0600

    first attempt at adding gic blocker variables

---
+++ src/prob/gmd_blocker_placement.jl
@@ -0,0 +1,41 @@
+export run_gmd
+
+
+"FUNCTION: run GIC current model"
+function run_gmd(file, optimizer; kwargs...)
+    return _PM.run_model(
+        file,
+        _PM.ACPPowerModel,
+        optimizer,
+        build_gmd;
+        ref_extensions = [
+            ref_add_gmd!
+        ],
+        solution_processors = [
+            solution_gmd!
+        ],
+        kwargs...,
+    )
+end
+
+
+"FUNCTION: build the quasi-dc power flow problem
+as a linear constraint satisfaction problem"
+function build_gmd(pm::_PM.AbstractPowerModel; kwargs...)
+
+
+    variable_gic_blocker_placement_on_off(pm)
+    variable_gic_blocker_on_off(pm)
+    variable_dc_voltage(pm)
+    variable_dc_line_flow(pm)
+
+    for i in _PM.ids(pm, :gmd_bus)
+        constraint_dc_power_balance_shunt(pm, i)
+    end
+
+    for i in _PM.ids(pm, :gmd_branch)
+        constraint_dc_ohms(pm, i)
+    end
+
+end
+
