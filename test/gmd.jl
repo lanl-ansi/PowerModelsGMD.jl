e3d6f5935633d522f0f22c0efbc8780030fb2013
Adam Mate <amate@lanl.gov>
2022-11-14 07:17:21 -0700

    update gmd.jl tests

--- test/gmd.jl
+++ test/gmd.jl
@@ -1,66 +1,48 @@
 @testset "TEST GMD" begin
 
 
-    # ===   B4GIC   === #
-
     @testset "B4GIC case" begin
 
         result = _PMGMD.run_gmd(case_b4gic, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
 
-        # - DC solution - %
-
+        # DC solution:
         dc_solution = result["solution"]
-
         @test isapprox(dc_solution["gmd_bus"]["3"]["gmd_vdc"], -32.0081, atol=1e-1)
-
         @test isapprox(dc_solution["gmd_branch"]["2"]["gmd_idc"], 106.6935, atol=1e-1)
 
     end
 
-
-
-    # ===   NERC B6GIC   === #
 
     @testset "NERC B6GIC case" begin
 
         result = _PMGMD.run_gmd(case_b6gic_nerc, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
 
-        # - DC solution - %
-
+        # DC solution:
         dc_solution = result["solution"]
-
         @test isapprox(dc_solution["gmd_bus"]["5"]["gmd_vdc"], -23.0222, atol=1e-1)
-
         @test isapprox(dc_solution["gmd_branch"]["3"]["gmd_idc"], -13.5072, atol=1e-1)
 
     end
 
-
-
-    # ===   EPRI21   === #
 
     @testset "EPRI21 case" begin
 
         result = _PMGMD.run_gmd(case_epri21, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
 
-        # - DC solution - %
-
+        # DC solution:
         dc_solution = result["solution"]
-
         @test isapprox(dc_solution["gmd_bus"]["5"]["gmd_vdc"], -6.5507, atol=1e-1)
         @test isapprox(dc_solution["gmd_bus"]["14"]["gmd_vdc"], 44.2630, atol=1e-1)
         @test isapprox(dc_solution["gmd_bus"]["17"]["gmd_vdc"], -40.6570, atol=1e-1)
-    
         @test isapprox(dc_solution["gmd_branch"]["5"]["gmd_idc"], 140.6257, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["13"]["gmd_idc"], 53.3282, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["29"]["gmd_idc"], 177.0521, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["35"]["gmd_idc"], -54.5694, atol=1e-1)
 
     end
-
 
 
 end
