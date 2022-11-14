e1cf593b3f0f976587f10c924d29f02d03383f2d
Adam Mate <amate@lanl.gov>
2022-11-14 07:00:09 -0700

    update to solve_ac_opf
    PMs function run_ac_opf has been replaced with solve_ac_opf

--- test/data_ac.jl
+++ test/data_ac.jl
@@ -3,36 +3,36 @@
 
     @testset "B4GIC case" begin
 
-        result = _PM.run_ac_opf(case_b4gic, ipopt_solver)
+        result = _PM.solve_ac_opf(case_b4gic, ipopt_solver)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
-        @test isapprox(result["objective"], 116384.9314; atol=1e3)
+        @test isapprox(result["objective"], 116384.9314; atol=1e2)
 
     end
 
 
     @testset "B4GIC-3W case" begin
 
-        result = _PM.run_ac_opf(case_b4gic3w, ipopt_solver)
+        result = _PM.solve_ac_opf(case_b4gic3w, ipopt_solver)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
-        @test isapprox(result["objective"], 116384.9314; atol=1e3)
+        @test isapprox(result["objective"], 116384.9314; atol=1e2)
 
     end
 
 
     @testset "B6GIC-NERC case" begin
 
-        result =_PM.run_ac_opf(case_b6gic_nerc, ipopt_solver; setting=setting)
+        result =_PM.solve_ac_opf(case_b6gic_nerc, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
-        @test isapprox(result["objective"], 977.3899; atol=1e3)
+        @test isapprox(result["objective"], 977.3899; atol=1e2)
 
     end
 
 
     @testset "EPRI21 case" begin
 
-        result = _PM.run_ac_opf(case_epri21, ipopt_solver; setting=setting)
+        result = _PM.solve_ac_opf(case_epri21, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
-        @test isapprox(result["objective"], 401802.4215; atol=1e3)
+        @test isapprox(result["objective"], 401802.4215; atol=1e2)
 
     end
 
