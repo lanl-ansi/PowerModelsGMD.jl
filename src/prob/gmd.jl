abfe56c80363f7367b4c13f8a85febb1e7b18a3c
Adam Mate <amate@lanl.gov>
2023-02-11 15:24:53 -0700

    update gmd.jl

--- src/prob/gmd.jl
+++ src/prob/gmd.jl
@@ -23,8 +23,8 @@ end
 as a linear constraint satisfaction problem"
 function build_gmd(pm::_PM.AbstractPowerModel; kwargs...)
 
-    variable_dc_voltage(pm; bounded=true)
-    variable_dc_line_flow(pm; bounded=true)
+    variable_dc_voltage(pm)
+    variable_dc_line_flow(pm)
 
     for i in _PM.ids(pm, :gmd_bus)
         constraint_dc_power_balance(pm, i)
