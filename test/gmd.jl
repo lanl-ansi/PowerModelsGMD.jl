f5d61a693e4d1948b0004754e82ea5c2eb49586b
Adam Mate <amate@lanl.gov>
2023-02-13 15:46:04 -0700

    merge gmd_matrix.jl into gmd.jl

--- test/gmd.jl
+++ test/gmd.jl
@@ -11,6 +11,17 @@
         @test isapprox(dc_solution["gmd_bus"]["3"]["gmd_vdc"], -32.0081, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["2"]["gmd_idc"], 106.6935, atol=1e-1)
 
+        # ===  === #
+
+        result = _PMGMD.solve_gmd(case_b4gic; setting=setting)
+        @test result["status"] == :LocalOptimal
+
+        # DC solution:
+        dc_solution = result["solution"]
+        @test isapprox(dc_solution["gmd_bus"]["3"]["gmd_vdc"], -32.0081, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["2"]["gmd_idc"], -63.9691, atol=1e-1)
+
+
     end
 
 
@@ -25,8 +36,20 @@
         _PMGMD.apply_mods!(b4gic3w_data, mods)
         _PMGMD.fix_gmd_indices!(b4gic3w_data)
 
+        # ===  === #
+
         result = _PMGMD.solve_gmd(b4gic3w_data, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
+
+        # DC solution:
+        dc_solution = result["solution"]
+        @test isapprox(dc_solution["gmd_bus"]["3"]["gmd_vdc"], -35.9637, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["2"]["gmd_idc"], -103.8494, atol=1e-1)
+
+        # ===  === #
+
+        result = _PMGMD.solve_gmd(b4gic3w_data; setting=setting)
+        @test result["status"] == :LocalOptimal
 
         # DC solution:
         dc_solution = result["solution"]
@@ -40,6 +63,16 @@
 
         result = _PMGMD.solve_gmd(case_b6gic_nerc, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
+
+        # DC solution:
+        dc_solution = result["solution"]
+        @test isapprox(dc_solution["gmd_bus"]["5"]["gmd_vdc"], -23.0222, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["3"]["gmd_idc"], -13.5072, atol=1e-1)
+
+        # ===  === #
+
+        result = _PMGMD.solve_gmd(case_b6gic_nerc; setting=setting)
+        @test result["status"] == :LocalOptimal
 
         # DC solution:
         dc_solution = result["solution"]
@@ -61,6 +94,21 @@
         @test isapprox(dc_solution["gmd_bus"]["17"]["gmd_vdc"], -40.6570, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["5"]["gmd_idc"], 140.6257, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["13"]["gmd_idc"], 53.3282, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["29"]["gmd_idc"], 177.0521, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["35"]["gmd_idc"], -54.5694, atol=1e-1)
+
+        # ===  === #
+
+        result = _PMGMD.solve_gmd(case_epri21; setting=setting)
+        @test result["status"] == :LocalOptimal
+
+        # DC solution:
+        dc_solution = result["solution"]
+        @test isapprox(dc_solution["gmd_bus"]["5"]["gmd_vdc"], -6.5507, atol=1e-1)
+        @test isapprox(dc_solution["gmd_bus"]["14"]["gmd_vdc"], 44.2630, atol=1e-1)
+        @test isapprox(dc_solution["gmd_bus"]["17"]["gmd_vdc"], -40.6569, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["5"]["gmd_idc"], -51.9651, atol=1e-1)
+        @test isapprox(dc_solution["gmd_branch"]["13"]["gmd_idc"], -15.0549, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["29"]["gmd_idc"], 177.0521, atol=1e-1)
         @test isapprox(dc_solution["gmd_branch"]["35"]["gmd_idc"], -54.5694, atol=1e-1)
 
