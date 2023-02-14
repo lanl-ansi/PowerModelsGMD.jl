3832bd12d3566911e0879c30d2199e379a9dc77f
Adam Mate <amate@lanl.gov>
2023-02-14 09:59:34 -0700

    update data_ac.jl test

--- test/data_ac.jl
+++ test/data_ac.jl
@@ -3,6 +3,8 @@
 
     @testset "B4GIC case" begin
 
+        case_b4gic = _PM.parse_file(data_b4gic)
+
         result = _PM.solve_ac_opf(case_b4gic, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
         @test isapprox(result["objective"], 116384.9314; atol=1e2)
@@ -11,6 +13,8 @@
 
 
     @testset "B4GIC-3W case" begin
+
+        case_b4gic3w = _PM.parse_file(data_b4gic3w)
 
         result = _PM.solve_ac_opf(case_b4gic3w, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
@@ -21,6 +25,8 @@
 
     @testset "B6GIC-NERC case" begin
 
+        case_b6gic_nerc = _PM.parse_file(data_b6gic_nerc)
+
         result =_PM.solve_ac_opf(case_b6gic_nerc, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
         @test isapprox(result["objective"], 977.3898; atol=1e2)
@@ -29,6 +35,8 @@
 
 
     @testset "EPRI21 case" begin
+
+        case_epri21 = _PM.parse_file(data_epri21)
 
         result = _PM.solve_ac_opf(case_epri21, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
@@ -39,7 +47,9 @@
 
     @testset "IEEE-RTS case" begin
 
-        result = _PM.solve_ac_opf(case24_ieee_rts_0, ipopt_solver; setting=setting)
+        case_ieee_rts_0 = _PM.parse_file(data_ieee_rts_0)
+
+        result = _PM.solve_ac_opf(case_ieee_rts_0, ipopt_solver; setting=setting)
         @test result["termination_status"] == _PM.LOCALLY_SOLVED
         @test isapprox(result["objective"], 64439.3409; atol=1e2)
 
@@ -47,6 +57,8 @@
 
 
     @testset "OTS-TEST case" begin  ## LOCALLY INFEASIBLE
+
+        case_otstest = _PM.parse_file(data_otstest)
 
         result = _PM.solve_ac_opf(case_otstest, ipopt_solver; setting=setting)
         # @test result["termination_status"] == _PM.LOCALLY_SOLVED
