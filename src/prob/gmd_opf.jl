28deecf2dce88bba69a161045e84a88cb0cec021
Adam Mate <amate@lanl.gov>
2023-02-19 16:17:37 -0700

    update gmd_opf.jl

--- src/prob/gmd_opf.jl
+++ src/prob/gmd_opf.jl
@@ -110,12 +110,7 @@ function solve_ac_gmd_opf_decoupled(file::String, optimizer; setting=Dict(), kwa
 end
 
 function solve_ac_gmd_opf_decoupled(case::Dict{String,Any}, optimizer; setting=Dict(), kwargs...)
-    return solve_gmd_opf_decoupled(
-        case,
-        _PM.ACPPowerModel,
-        optimizer;
-        kwargs...
-    )
+    return solve_gmd_opf_decoupled(case, _PM.ACPPowerModel, optimizer; kwargs...)
 end
 
 function solve_gmd_opf_decoupled(file::String, model_type, optimizer; setting=Dict(), kwargs...)
@@ -136,7 +131,7 @@ function solve_gmd_opf_decoupled(dc_case::Dict{String,Any}, model_type, optimize
         dc_current_mag(branch, ac_case, dc_solution)
     end
 
-    PowerModelsGMD.qloss_decoupled_vnom(ac_case)
+    qloss_decoupled_vnom(ac_case)
     # ac_result = solve_opf_qloss_vnom(ac_case, model_type, optimizer, setting=setting)
     ac_result = _PM.solve_opf(ac_case, model_type, optimizer, setting=setting)
     ac_solution = ac_result["solution"]
@@ -169,9 +164,9 @@ function solve_gmd_opf(file, model_type::Type, optimizer; kwargs...)
             ref_add_gmd!
         ],
         solution_processors = [
-            solution_gmd!,
             solution_PM!,
             solution_gmd_qloss!,
+            solution_gmd!,
             solution_gmd_mls!
         ],
         kwargs...,
