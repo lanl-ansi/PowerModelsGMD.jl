1af8f9f15a40e16446021f361a61865e9cec8541
Adam Mate <amate@lanl.gov>
2023-02-28 07:32:36 -0700

    update gmd_ots.jl test

--- test/gmd_ots.jl
+++ test/gmd_ots.jl
@@ -4,7 +4,7 @@
     @testset "EPRI21 case" begin
 
 
-        # ===   DECOUPLED GMD MLD   === #
+        # ===   COUPLED AC-MLS-OTS   === #
 
 
         # case_epri21 = _PM.parse_file(data_epri21)
@@ -30,13 +30,21 @@
         # FIXME: add actual fully automated testing for "solve_ac_gmd_mls_ots"
 
 
+        # ===   COUPLED AC-MLS-OTS-TS   === #
+
+        
+        # FIXME: add actual fully automated testing for "solve_soc_gmd_mls_ots_ts"
+        
+        # FIXME: add actual fully automated testing for "solve_ac_gmd_mls_ots_ts"
+
+
     end
 
 
     @testset "OTS-TEST case" begin
 
 
-        # ===   DECOUPLED GMD MLD   === #
+        # ===   COUPLED AC-MLS-OTS   === #
 
 
         # case_otstest = _PM.parse_file(data_otstest)
@@ -60,6 +68,14 @@
         # @test isapprox(result["objective"], 0.0000; atol=1e2)
 
         # FIXME: add actual fully automated testing for "solve_ac_gmd_mls_ots"
+
+
+        # ===   COUPLED AC-MLS-OTS-TS   === #
+
+        
+        # FIXME: add actual fully automated testing for "solve_soc_gmd_mls_ots_ts"
+        
+        # FIXME: add actual fully automated testing for "solve_ac_gmd_mls_ots_ts"
 
 
     end
