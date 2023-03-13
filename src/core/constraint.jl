c6d1a40846860091e64e8bd01a9dfe2918b8b3d2
Russell Bent <rbent@lanl.gov>
2023-03-13 17:24:28 -0600

    constraint_dc_current_mag_gwye_gwye_xf
    Made the relaxation of constraint_dc_current_mag_gwye_gwye_xf no longer the default implementation.  Now, if a user tries to use a model for which there is no implementation constraint_dc_current_mag_gwye_gwye_xf, the code will produce an error message to indicate that such an implementation needs to be created to force the user to make a decision about what version of the constraint is appropriate for the model.

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -90,22 +90,8 @@ end
 "CONSTRAINT: dc current on ungrounded gwye-gwye transformers"
 function constraint_dc_current_mag_gwye_gwye_xf(pm::_PM.AbstractPowerModel, n::Int, k, kh, ih, jh, kl, il, jl, a)
 
-    Memento.debug(_LOGGER, "branch[$k]: hi_branch[$kh], lo_branch[$kl]")
-
-    ieff = _PM.var(pm, n, :i_dc_mag)[k]
-    ihi = _PM.var(pm, n, :dc)[(kh,ih,jh)]
-    ilo = _PM.var(pm, n, :dc)[(kl,il,jl)]
-
-    JuMP.@constraint(pm.model,
-        ieff
-        >=
-        (a * ihi + ilo) / a
-    )
-    JuMP.@constraint(pm.model,
-        ieff
-        >=
-        - (a * ihi + ilo) / a
-    )
+    type = typeof(pm)
+    Memento.error(_LOGGER, "Error: Function constraint_dc_current_mag_gwye_gwye_xf needs to be implemented for PowerModel of type $type")
 
 end
 
--- src/form/dcp.jl
+++ src/form/dcp.jl
@@ -220,3 +220,26 @@ function constraint_dc_current_mag_gwye_delta_xf(pm::_PM.AbstractDCPModel, n::In
     )
 
 end
+
+
+"CONSTRAINT: dc current on ungrounded gwye-gwye transformers"
+function constraint_dc_current_mag_gwye_gwye_xf(pm::_PM.AbstractDCPModel, n::Int, k, kh, ih, jh, kl, il, jl, a)
+
+    Memento.debug(_LOGGER, "branch[$k]: hi_branch[$kh], lo_branch[$kl]")
+
+    ieff = _PM.var(pm, n, :i_dc_mag)[k]
+    ihi = _PM.var(pm, n, :dc)[(kh,ih,jh)]
+    ilo = _PM.var(pm, n, :dc)[(kl,il,jl)]
+
+    JuMP.@constraint(pm.model,
+        ieff
+        >=
+        (a * ihi + ilo) / a
+    )
+    JuMP.@constraint(pm.model,
+        ieff
+        >=
+        - (a * ihi + ilo) / a
+    )
+
+end
