4fd4e5d2dfdd1468cec58899ee725229a6292745
Russell Bent <rbent@lanl.gov>
2023-03-15 14:00:15 -0600

    Turned on two mld tests

--- test/gmd_mld.jl
+++ test/gmd_mld.jl
@@ -84,30 +84,15 @@
 
         # ===   COUPLED GMD MLS   === #
 
+        case_ieee_rts_0 = _PM.parse_file(data_ieee_rts_0)
+        result = _PMGMD.solve_soc_gmd_mld(case_ieee_rts_0, ipopt_solver; setting=setting)
+        @test result["termination_status"] == _PM.LOCALLY_SOLVED
+        @test isapprox(result["objective"], 295.0; atol=1e-2)
 
         case_ieee_rts_0 = _PM.parse_file(data_ieee_rts_0)
-
-        #FIXME: The lower bound (soc) solution is coming back worse than the AC solution.  Seems to be something wonky going on
-        # with the dc constraints
-
-        #result = _PMGMD.solve_soc_gmd_mld(case_ieee_rts_0, ipopt_solver; setting=setting)
-        #@test result["termination_status"] == _PM.LOCALLY_SOLVED
-        #@test isapprox(result["objective"], 0.0; atol=1e-2)
-
-
-         case_ieee_rts_0 = _PM.parse_file(data_ieee_rts_0)
-
-         # FIXME - requires fix in powermodelsrestoration for qc bug
-         #result = _PMGMD.solve_qc_gmd_mld(case_ieee_rts_0, ipopt_solver; setting=setting)
-         #@test result["termination_status"] == _PM.LOCALLY_SOLVED
-         #@test isapprox(result["objective"], 0.0000; atol=1e2)
-
-
-        case_ieee_rts_0 = _PM.parse_file(data_ieee_rts_0)
-
-        #result = _PMGMD.solve_ac_gmd_mld(case_ieee_rts_0, ipopt_solver; setting=setting)
-        #@test result["termination_status"] == _PM.LOCALLY_SOLVED
-        #@test isapprox(result["objective"], 0.0; atol=1e-2)
+        result = _PMGMD.solve_ac_gmd_mld(case_ieee_rts_0, ipopt_solver; setting=setting)
+        @test result["termination_status"] == _PM.LOCALLY_SOLVED
+        @test isapprox(result["objective"], 282.5109; atol=1e-2)
 
     end
 
