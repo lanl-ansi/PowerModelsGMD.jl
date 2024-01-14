f7b5cce2c085f6907596a66796838f0cffbfaed0
Art Barnes <abarnes@lanl.gov>
2024-01-14 11:07:00 -0700

    add updated test cases

---
+++ test/test_cases.jl
@@ -0,0 +1,26 @@
+
+# # Parse test cases:
+# data_b4gic = "../test/data/matpower/b4gic.m"
+# # data_b4gic3w = "../test/data/pti/b4gic3w.raw"
+# data_b6gic_nerc = "../test/data/matpower/b6gic_nerc.m"
+# data_epri21 = "../test/data/matpower/epri21.m"
+# data_ieee_rts_0 = "../test/data/matpower/ieee_rts_0.m"
+# data_otstest = "../test/data/matpower/ots_test.m"
+# data_b4gic_ne_blocker = "../test/data/matpower/b4gic_ne_blocker.m"
+# data_epri21_ne_blocker = "../test/data/matpower/epri21_ne_blocker.m"
+# data_b4gic_15kvm_contingency = "../test/data/matpower/b4gic_15kvm_contingency.m"
+# data_epri21_15kvm_contingency = "../test/data/matpower/epri21_15kvm_contingency.m"
+# # data_uiuc_150bus = "../test/data/pti/uiuc-150bus.raw"
+# # data_uiuc_150bus_contigency_mods = "../test/data/suppl/uiuc-150bus_15kvm_contigency_mods.json"
+# data_blocker_test = "../test/data/matpower/blocker_test.m"
+
+# data_b4gic_verification = "../test/data/verification/B4GIC_Slack_Only_GICXformer_1Vkm.csv"
+# data_epri21_verification = "../test/data/verification/EpriCase_Mar2012_GICXformer_1Vkm.csv"
+
+# data_b4gic_vm = "../test/data/verification/B4GIC_Slack_Only_Bus_1Vkm.csv"
+# data_epri21_vm = "../test/data/verification/EpriCase_Mar2012_Bus_1Vkm.csv"
+b4gic_default = "../test/data/matpower/b4gic_default.m"
+b4gic_offbase = "../test/data/matpower/b4gic_offbase.m"
+autotransformer = "../test/data/matpower/autotransformer.m"
+epricase = "../test/data/matpower/epricase_aug2022_v22_fix.m"
+
