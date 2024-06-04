5beb52e2219b551a857ef142dabe5eec19eb612d
Art Barnes <art@pin2.io>
2024-06-04 13:55:56 +0200

    add ieff_max into constraints

--- src/form/acp.jl
+++ src/form/acp.jl
@@ -22,35 +22,35 @@ end
 
 
 "CONSTRAINT: dc current on ungrounded gwye-delta transformers"
-function constraint_dc_current_mag_gwye_delta_xf(pm::_PM.AbstractACPModel, n::Int, k, kh, ih, jh)
+function constraint_dc_current_mag_gwye_delta_xf(pm::_PM.AbstractACPModel, n::Int, k, kh, ih, jh, ieff_max)
 
     ieff = _PM.var(pm, n, :i_dc_mag)[k]
     ihi = _PM.var(pm, n, :dc)[(kh,ih,jh)]
     JuMP.@NLconstraint(pm.model, ieff == abs(ihi))
-
+    JuMP.@constraint(pm.model, ieff <= ieff_max)
 end
 
 
 "CONSTRAINT: dc current on ungrounded gwye-gwye transformers"
-function constraint_dc_current_mag_gwye_gwye_xf(pm::_PM.AbstractACPModel, n::Int, k, kh, ih, jh, kl, il, jl, a)
+function constraint_dc_current_mag_gwye_gwye_xf(pm::_PM.AbstractACPModel, n::Int, k, kh, ih, jh, kl, il, jl, a, ieff_max)
 
     Memento.debug(_LOGGER, "branch[$k]: hi_branch[$kh], lo_branch[$kl]")
     ieff = _PM.var(pm, n, :i_dc_mag)[k]
     ihi = _PM.var(pm, n, :dc)[(kh,ih,jh)]
     ilo = _PM.var(pm, n, :dc)[(kl,il,jl)]
     JuMP.@NLconstraint(pm.model, ieff == abs(a*ihi + ilo)/a)
-
+    JuMP.@constraint(pm.model, ieff <= ieff_max)
 end
 
 
 "CONSTRAINT: dc current on ungrounded gwye-gwye auto transformers"
-function constraint_dc_current_mag_gwye_gwye_auto_xf(pm::_PM.AbstractACPModel, n::Int, k, ks, is, js, kc, ic, jc, a)
+function constraint_dc_current_mag_gwye_gwye_auto_xf(pm::_PM.AbstractACPModel, n::Int, k, ks, is, js, kc, ic, jc, a, ieff_max)
 
     ieff = _PM.var(pm, n, :i_dc_mag)[k]
     is = _PM.var(pm, n, :dc)[(ks,is,js)]
     ic = _PM.var(pm, n, :dc)[(kc,ic,jc)]
     JuMP.@NLconstraint(pm.model, ieff == abs(a*is + ic)/(a + 1.0))
-
+    JuMP.@constraint(pm.model, ieff <= ieff_max)
 end
 
 
