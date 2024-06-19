4232b946b0dcea9dcd08cde4acc1992de80f5255
Aniruddh Mishra <aniruddhm17@gmail.com>
2024-06-19 07:57:34 -0600

    Unit Tests for GIC Parser

---
+++ test/gic.jl
@@ -0,0 +1,117 @@
+# Test cases for GIC file parser
+
+@testset "test .gic file parser" begin
+    @testset "Parse Example GIC files" begin
+        Memento.setlevel!(TESTLOG, "warn")
+        @test_nowarn PowerModelsGMD.parse_gic("../test/data/gic/EPRI.gic")
+        Memento.setlevel!(TESTLOG, "error")
+    end
+    @testset "Check GIC parser rrors" begin
+        Memento.setlevel!(TESTLOG, "warn")
+        @test_throws(TESTLOG, "This parser only interprets GIC Version 4. Please ensure you are using the correct format.",
+        PowerModelsGMD.parse_gic("../test/data/gic/WrongVersion.gic"))
+        @test_throws(TESTLOG, "At line 2, the number of \"'\" characters are mismatched. Please make sure you close all your strings.",
+        PowerModelsGMD.parse_gic("../test/data/gic/MisMatchedQuotes.gic"))
+        # TODO: Parsing error
+        Memento.setlevel!(TESTLOG, "error")
+    end
+    @testset "Check GIC parser warnings" begin
+        @testset "Warns about missing parsing for EARTH MODEL and LOAD data" begin
+            Memento.setlevel!(TESTLOG, "warn")
+            @test_warn(TESTLOG, "EARTH MODEL data is not supported by this parser and will be ignored.",
+            PowerModelsGMD.parse_gic("../test/data/gic/EarthModel.gic"))
+            @test_warn(TESTLOG, "LOAD data is not supported by this parser and will be ignored.",
+            PowerModelsGMD.parse_gic("../test/data/gic/LoadData.gic"))
+            Memento.setlevel!(TESTLOG, "error")
+        end
+        @testset "Warns about incorrect formatting of data" begin
+            Memento.setlevel!(TESTLOG, "warn")
+            @test_warn(TESTLOG, "At line 2, SUBSTATION has extra fields, which will be ignored.",
+            PowerModelsGMD.parse_gic("../test/data/gic/ExcessElements.gic"))
+            @test_warn(TESTLOG, "At line 6, BUS is missing SUBSTATION, which will be set to default.",
+            PowerModelsGMD.parse_gic("../test/data/gic/MissingField.gic"))
+            @test_warn(TESTLOG, "At line 6, BUS is missing SUBSTATION, which will be set to default.",
+            PowerModelsGMD.parse_gic("../test/data/gic/MissingField.gic"))
+            # Parse warning
+            @test_warn(TESTLOG, "At line 9, section BUS started with '0', but additional non-comment data is present. Pattern '^\\s*0\\s*[/]*.*' is reserved for section start/end.",
+            PowerModelsGMD.parse_gic("../test/data/gic/IncorrectSectionStart.gic"))
+            @test_warn(TESTLOG, "Too many sections at line 23. Please ensure you don't have any extra sections.",
+            PowerModelsGMD.parse_gic("../test/data/gic/ExtraSection.gic"))
+            Memento.setlevel!(TESTLOG, "error")
+        end
+    end
+    @testset "Bus4 file" begin
+        data_dict = PowerModelsGMD.parse_gic("../test/data/gic/Bus4.gic")
+        @test isa(data_dict, Dict)
+
+        @test length(data_dict["SUBSTATION"]) == 2
+        for item in data_dict["SUBSTATION"]
+            @test length(item) == 8
+        end
+
+        @test length(data_dict["BUS"]) == 4
+        for item in data_dict["BUS"]
+            @test length(item) == 2
+        end
+
+        @test length(data_dict["TRANSFORMER"]) == 2
+        for item in data_dict["TRANSFORMER"]
+            @test length(item) == 17
+        end
+
+        @test length(data_dict["BRANCH"]) == 1
+        for item in data_dict["BRANCH"]
+            @test length(item) == 8
+        end
+    end
+
+    @testset "EPRI file" begin
+        data_dict = PowerModelsGMD.parse_gic("../test/data/gic/EPRI.gic")
+        @test isa(data_dict, Dict)
+
+        @test length(data_dict["SUBSTATION"]) == 8
+        for item in data_dict["SUBSTATION"]
+            @test length(item) == 8
+        end
+
+        @test length(data_dict["BUS"]) == 19
+        for item in data_dict["BUS"]
+            @test length(item) == 2
+        end
+
+        @test length(data_dict["TRANSFORMER"]) == 15
+        for item in data_dict["TRANSFORMER"]
+            @test length(item) == 17
+        end
+
+        @test length(data_dict["BRANCH"]) == 16
+        for item in data_dict["BRANCH"]
+            @test length(item) == 8
+        end
+    end
+
+    @testset "Defaults file" begin
+        data_dict = PowerModelsGMD.parse_gic("../test/data/gic/Defaults.gic")
+        @test isa(data_dict, Dict)
+
+        @test length(data_dict["SUBSTATION"]) == 2
+        for item in data_dict["SUBSTATION"]
+            @test length(item) == 8
+        end
+
+        @test length(data_dict["BUS"]) == 4
+        for item in data_dict["BUS"]
+            @test length(item) == 2
+        end
+
+        @test length(data_dict["TRANSFORMER"]) == 2
+        for item in data_dict["TRANSFORMER"]
+            @test length(item) == 17
+        end
+
+        @test length(data_dict["BRANCH"]) == 1
+        for item in data_dict["BRANCH"]
+            @test length(item) == 8
+        end
+    end
+end
