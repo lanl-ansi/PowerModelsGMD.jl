699e43027b878cff098a3ba6b91810e76d858205
Art Barnes <abarnes@lanl.gov>
2024-08-02 08:14:59 -0600

    add placed blockers for epri21

--- test/data/matpower/epri21_blocker_placement.m
+++ test/data/matpower/epri21_blocker_placement.m
@@ -374,13 +374,13 @@ mpc.branch_sourceid = [
 %% gmd_blocker data
 %column_names% gmd_bus status construction_cost
 mpc.gmd_ne_blocker = {
-	1	  0 1.0
+	1	  1 1.0
 	2	  1	1.0
-	3	  0	1.0
+	3	  1	1.0
 	4	  1	1.0
-	5	  0	1.0
+	5	  1	1.0
 	6	  1	1.0
-	7	  0	1.0
+	7	  1	1.0
 	8	  1	1.0
 };
 
