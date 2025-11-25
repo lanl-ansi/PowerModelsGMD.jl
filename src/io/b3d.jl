76a53761e2b83cc87b04af81409623da2b33f036
Art Barnes <abarnes@lanl.gov>
2025-11-25 10:16:38 -0700

    fix bug in writer

--- src/io/b3d.jl
+++ src/io/b3d.jl
@@ -1,3 +1,7 @@
+const _LOGGER = Memento.getlogger(@__MODULE__)
+__init__() = Memento.register(_LOGGER)
+Memento.setlevel!(_LOGGER, "debug")
+
 B3D_MAGIC_NUMBER = UInt32(34280)
 
 function read_byte(io)
@@ -5,7 +9,7 @@ function read_byte(io)
 end
 
 function write_byte(io, b)
-    return write(i, b)
+    return write(io, b)
 end
 
 function read_uint32(io)
@@ -51,7 +55,7 @@ function write_null_terminated_string(io, buf)
         write_byte(io, convert(UInt8, b))
     end
 
-    write_byte(io, convert(Uint8, 0))
+    write_byte(io, convert(UInt8, 0))
 end
 
 
@@ -59,7 +63,7 @@ function read_b3d_header(io::IO)
     b3d = Dict()
     magic_number = read_uint32(io)
 
-    Memento.debug(_LOGGER, "Magic number: $magic_number")
+    Memento.info(_LOGGER, "Magic number: $magic_number")
 
     if magic_number != B3D_MAGIC_NUMBER
         throw(ErrorException("Invalid B3D file with magic number $magic_number, expecting $B3D_MAGIC_NUMBER"))
@@ -67,15 +71,15 @@ function read_b3d_header(io::IO)
 
     b3d_version = read_uint32(io)
     Memento.info(_LOGGER, "B3D version: $b3d_version")
-    b3d["version"] = b3d_version
+    b3d["version"] = convert(Int64, b3d_version)
 
     if b3d_version != 4
         throw(ErrorException("Version $b3d_version is not supported. Only version 4 is supported"))
     end
 
     n_meta_strings =  read_uint32(io)
-    Memento.debug(_LOGGER, "Number of metadata strings: $n_meta_strings")
-    b3d["n_meta_strings"] = n_meta_strings
+    Memento.info(_LOGGER, "Number of metadata strings: $n_meta_strings")
+    b3d["n_meta_strings"] = convert(Int64, n_meta_strings)
 
     meta_strings = []
 
@@ -90,16 +94,16 @@ function read_b3d_header(io::IO)
     b3d["comments"] = comments
 
     n_float_channels = read_uint32(io)
-    Memento.debug(_LOGGER, "Number of float channels: $n_float_channels")
-    b3d["n_float_channels"] = n_float_channels
+    Memento.info(_LOGGER, "Number of float channels: $n_float_channels")
+    b3d["n_float_channels"] = convert(Int64, n_float_channels)
 
     if n_float_channels < 2
         throw(ErrorException("Unspported number of float channels $n_float_channels, only B3D files with at least 2 float channels are supported"))
     end
 
     n_byte_channels = read_uint32(io)
-    Memento.debug(_LOGGER, "Number of byte channels: $n_byte_channels")
-    b3d["n_byte_channels"] = n_byte_channels
+    Memento.info(_LOGGER, "Number of byte channels: $n_byte_channels")
+    b3d["n_byte_channels"] = convert(Int64, n_byte_channels)
 
     if n_byte_channels >= 1
         throw(ErrorException("File contains $n_byte_channels byte channels which are currently unspported"))
@@ -107,7 +111,7 @@ function read_b3d_header(io::IO)
  
     loc_format = read_uint32(io)
     Memento.info(_LOGGER, "Location format: $loc_format")
-    b3d["loc_format"] = loc_format
+    b3d["loc_format"] = convert(Int64, loc_format)
     
     if loc_format != 1
         throw(ErrorException("Unspported location format $loc_format, only location format 1 is supported"))
@@ -115,7 +119,7 @@ function read_b3d_header(io::IO)
 
     n_points = read_uint32(io)
     Memento.info(_LOGGER, "Number of points: $n_points")
-    b3d["n_points"] = n_points
+    b3d["n_points"] = convert(Int64, n_points)
 
     lat = zeros(n_points)
     lon = zeros(n_points)
@@ -137,8 +141,8 @@ function read_b3d_header(io::IO)
     b3d["dist_to_measurement_station"] = dist_to_measurement_station
 
     start_time = read_uint32(io)
-    Memento.debug(_LOGGER, "Start time in seconds since epoch: $start_time")
-    b3d["start_time"] = start_time
+    Memento.info(_LOGGER, "Start time in seconds since epoch: $start_time")
+    b3d["start_time"] = convert(Int64, start_time)
 
     # const (
     # 	nsTimeUnits = -2
@@ -150,24 +154,24 @@ function read_b3d_header(io::IO)
     time_units = Dict(-2 => 1e-9, -1 => 1e-6, 0 => 1e-3, 1 => 1.0)
 
     time_unit_code = read_uint32(io)
-    Memento.debug(_LOGGER, "Time unit: $time_unit_code")
-    # b3d["time_unit_code"] = time_unit_code
+    Memento.info(_LOGGER, "Time unit: $time_unit_code")
+    b3d["time_unit_code"] = convert(Int64, time_unit_code)
 
     time_unit = time_units[time_unit_code]
     Memento.info(_LOGGER, "Time unit in seconds: $time_unit")
     b3d["time_unit"] = time_unit
 
     time_offset_raw = read_uint32(io)
-    Memento.debug(_LOGGER, "Time offset in time units: $time_offset_raw")
-    # b3d["time_offset_raw"] = time_offset_raw
+    Memento.info(_LOGGER, "Time offset in time units: $time_offset_raw")
+    b3d["time_offset_raw"] = convert(Int64, time_offset_raw)
 
     time_offset = time_unit*convert(Float64, time_offset_raw)
     Memento.info(_LOGGER, "Time offset in seconds: $time_offset")
     b3d["time_offset"] = time_offset
 
     time_step_raw = read_uint32(io)
-    Memento.debug(_LOGGER, "Time step in raw units: $time_step_raw")
-    # b3d["time_step_raw"] = time_step_raw
+    Memento.info(_LOGGER, "Time step in raw units: $time_step_raw")
+    b3d["time_step_raw"] = convert(Int64, time_step_raw)
 
     time_step = time_unit*convert(Float64, time_step_raw)
     Memento.info(_LOGGER, "Time step in seconds: $time_step")
@@ -175,7 +179,7 @@ function read_b3d_header(io::IO)
 
     n_times = read_uint32(io)
     Memento.info(_LOGGER, "Number of time steps: $n_times")
-    b3d["n_times"] = n_times
+    b3d["n_times"] = convert(Int64, n_times)
 
     return b3d
 end
@@ -183,6 +187,7 @@ end
 
 function read_b3d(io::IO)
     b3d = Dict{String,Any}()
+    # TODO: pop lat/lon out of the header?
     b3d["header"] = read_b3d_header(io)
     n_times = b3d["header"]["n_times"]
     n_points = b3d["header"]["n_points"]
@@ -221,15 +226,16 @@ end
 
 function read_b3d(b3d_file::String)
     open(b3d_file) do io
-        b3d = read_b3d(io)
+        return read_b3d(io)
     end
-    return b3d
 end
 
 
 function write_b3d_header(io::IO, b3d)
+    header = b3d["header"]
+
     write_uint32(io, B3D_MAGIC_NUMBER)
-    b3d_version = b3d["version"]
+    b3d_version = header["version"]
 
     if b3d_version != 4
         throw(ErrorException("Version $b3d_version is not supported. Only version 4 is supported"))
@@ -237,27 +243,27 @@ function write_b3d_header(io::IO, b3d)
 
     write_uint32(io, b3d_version)
 
-    meta_strings = split(b3d["comment"], "\n")
+    meta_strings = split(header["comments"], "\n")
     n_meta_strings = length(meta_strings)
-    write_uint32(n_meta_strings)
+    write_uint32(io, n_meta_strings)
 
     for s in meta_strings
         write_null_terminated_string(io, s)
     end
 
-    write_uint32(b3d["n_float_channels"])
-    write_uint32(b3d["n_byte_channels"])
-    write_uint32(b3d["loc_format"])
-    write_uint32(b3d["n_points"])
+    write_uint32(io, header["n_float_channels"])
+    write_uint32(io, header["n_byte_channels"])
+    write_uint32(io, header["loc_format"])
+    write_uint32(io, header["n_points"])
 
 
-    for i in 1:b3d["n_points"]
-        lon[i] = write_float64(io, b3d["lon"][i])
-        lat[i] = write_float64(io, b3d["lat"][i])
-        write_float64(io, b3d["dist_to_measurement_station"][i])   
+    for i in 1:header["n_points"]
+        write_float64(io, header["lon"][i])
+        write_float64(io, header["lat"][i])
+        write_float64(io, header["dist_to_measurement_station"][i])   
     end
 
-    write_uint32(io, b3d["start_time"])
+    write_uint32(io, header["start_time"])
 
     # const (
     # 	nsTimeUnits = -2
@@ -268,26 +274,26 @@ function write_b3d_header(io::IO, b3d)
 
     time_units = Dict(-2 => 1e-9, -1 => 1e-6, 0 => 1e-3, 1 => 1.0)
     tu_codes = Dict(v => k for (k, v) in time_units)
-    time_unit = b3d["time_unit"] 
+    time_unit = header["time_unit"] 
     Memento.info(_LOGGER, "Time unit in seconds: $time_unit")
     time_unit_code = tu_codes[time_unit]
-    Memento.debug(_LOGGER, "Time unit: $time_unit_code")
+    Memento.info(_LOGGER, "Time unit: $time_unit_code")
     write_uint32(io, time_unit_code)
 
-    time_offset = b3d["time_offset"]
+    time_offset = header["time_offset"]
     Memento.info(_LOGGER, "Time offset in seconds: $time_offset")
     time_offset_raw = round(UInt32, time_offset/time_unit)
-    Memento.debug(_LOGGER, "Time offset in time units: $time_offset_raw")
+    Memento.info(_LOGGER, "Time offset in time units: $time_offset_raw")
     write_uint32(io, time_offset_raw)
 
-    time_step = b3d["time_step"]
+    time_step = header["time_step"]
     Memento.info(_LOGGER, "Time step in seconds: $time_step")
-    time_step_raw = round(Uint32, time_step/time_unit)
-    Memento.debug(_LOGGER, "Time step in raw units: $time_step_raw")
+    time_step_raw = round(UInt32, time_step/time_unit)
+    Memento.info(_LOGGER, "Time step in raw units: $time_step_raw")
     write_uint32(io, time_step)
   
     
-    write_uint32(io, b3d["n_times"])
+    write_uint32(io, header["n_times"])
 end
 
 
@@ -298,6 +304,16 @@ function write_b3d(io::IO, b3d)
     n_points = b3d["header"]["n_points"]
     Ex = b3d["Ex"]
     Ey = b3d["Ey"]
+
+    # TODO: scale/shift after converting to uint32
+    time_unit = b3d["header"]["time_unit"]
+    time_offset = b3d["header"]["time_offset"]
+
+    for t in b3d["t"]
+        # t[i] = time_unit*read_uint32(io) + time_offset
+        t_raw = round(UInt32, (t - time_offset)/time_unit)
+        write_uint32(io, t_raw)
+    end    
 
     Memento.info(_LOGGER, "Start writing electric field points")
 
